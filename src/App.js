56aa4488b08a29d5fd93a7d0e1e8cc336ab3f676
FiroozMohammadi <firoozmohammaid555@gmail.com>
2026-04-14 20:43:59 +0300

    Day 3 - Events and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Profile from './profile';
 
 function App() {
+const[message, setMessage]=useState("");
+  function onClick(){
+    // alert("done");
+    setMessage("done");
+  }
   return (
     <div>
       <h1>Create simple UI - Day 1</h1>
      <Profile fullname="Fahim Rahimi" username="fahim11"/>
+     <button onClick={onClick}>Click</button>
+     <button onClick={()=> setMessage("clicked")}>click button</button>
+     <p>{message}</p>
     </div>
   );
 }
