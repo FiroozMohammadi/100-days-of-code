589b3d101c03275293f51ec2c86af6a479d193e7
FiroozMohammadi <firoozmohammaid555@gmail.com>
2026-04-15 12:02:26 +0300

    Day 4 - Input and user typing

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Profile from './profile';
 
 function App() {
 const[message, setMessage]=useState("");
+const[name, setName]=useState("");
+const[input, setInput]=useState("");
+
+function handlChange(event){
+  setName(event.target.value);
+}
   function onClick(){
     // alert("done");
     setMessage("done");
   }
   return (
     <div>
-      <h1>Create simple UI - Day 1</h1>
+      <h1>Create simple UI - Day 4</h1>
      <Profile fullname="Fahim Rahimi" username="fahim11"/>
      <button onClick={onClick}>Click</button>
      <button onClick={()=> setMessage("clicked")}>click button</button>
      <p>{message}</p>
+     <input type='text' onChange={handlChange} placeholder='Enter Name' />
+     <input type='text' onChange={(e)=>setInput(e.target.value)} placeholder='Enter lastname' />
+     <button onClick={()=>setName(input)}>click</button>
+     <p>{name}</p>
     </div>
   );
 }
