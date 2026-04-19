1cdf4414a40ef05a8a0af76ee5c54cd0319c4893
FiroozMohammadi <firoozmohammaid555@gmail.com>
2026-04-19 12:42:39 +0300

    Day 8 - Save tasks with localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
- // const task=["learn react","solve problem","play football"];
 
-  const[tasks,setTasks]=useState([]);
+  const[tasks,setTasks]=useState(()=>{
+    const saved=localStorage.getItem("tasks");
+    return saved? JSON.parse(saved):[];
+  });
+  
+ useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks]);
+
   const[input, setInput]=useState("");
   const [editIndex, setEditIndex] = useState(null);
 
@@ -31,28 +38,11 @@ function App() {
     setInput(tasks[index]);
     setEditIndex(index);
   }
-// const[message, setMessage]=useState("");
-// const[name, setName]=useState("");
 
-
-// function handlChange(event){
-//   setName(event.target.value);
-// }
-//   function onClick(){
-//     // alert("done");
-//     setMessage("done");
-//   }
   return (
-    <div className='container'>
-      <h1>Styling clean UI - Day 7</h1>
-     {/* <Profile fullname="Fahim Rahimi" username="fahim11"/> */}
-     {/* <button onClick={onClick}>Click</button> */}
-     {/* <button onClick={()=> setMessage("clicked")}>click button</button> */}
-     {/* <p>{message}</p> */}
-     {/* <input type='text' onChange={handlChange} placeholder='Enter Name' /> */}
-     {/* <input type='text' onChange={(e)=>setInput(e.target.value)} placeholder='Enter lastname' /> */}
-     {/* <button onClick={()=>setName(input)}>click</button> */}
-     {/* <p>{name}</p> */}
+    <div className="container">
+      <h1>save item temporary - Day 8</h1>
+  
 
     
     
