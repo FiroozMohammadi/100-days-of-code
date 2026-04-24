9139526d95db5ff2e6efcfe9b5c1b6bc21ddb8f9
FiroozMohammadi <firoozmohammaid555@gmail.com>
2026-04-24 13:44:42 +0300

    Day 13 - Improve UI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 
     const[tasks,setTasks]=useState([]);
     const[input,setInput]=useState("");
+    const[editId, setEditId]=useState(null);
 
     const API_URL="https://localhost:7107/api/task";
     useEffect(()=>{
@@ -13,50 +14,93 @@ function App() {
       .then(data=>setTasks(data))
       .catch((err)=>console.log(err));
     },[]);
+function deleteTask(id) {
+  fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      fetch(API_URL)
+        .then(res => res.json())
+        .then(data => setTasks(data));
+    });
+}
 
-     function sendData() {
-    if (input.trim() === "") return;
+function editTask(task){
+  setInput(task.title);
+  setEditId(task.id);
+}
+     
+   
+  function sendData() {
+  if (input.trim() === "") return;
+if (editId !== null) {
+  fetch(`${API_URL}/${editId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title: input }),
+  })
+    .then(() => {
+      // reload data after update
+      fetch(API_URL)
+        .then(res => res.json())
+        .then(data => {
+          setTasks(data);
+          setInput("");
+          setEditId(null);
+        });
+    });
 
+  } 
+  else {
     fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({title: input}),
+      body: JSON.stringify({ title: input }),
     })
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         setTasks(data);
-        setInput("");  
-      })
-      .catch((err) => console.log(err));
+        setInput("");
+      });
   }
+}
 
 
   return (
-   <div className="container">
-        <h1>send and fetch data by API - Day 11</h1>
-  
+  <div className="container">
+    <h1>Task Manager</h1>
 
-    
-    
+    <div className="input-group">
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        placeholder="enter task"
+        placeholder="Enter task..."
       />
-     <button onClick={sendData}>
-       add task
-      </button>
 
-      <ul>
-        {tasks.map((task, index) => (
-  <li key={index}>{task.title}</li>
-))}
-      </ul>
-      </div>
-  );
+      <button onClick={sendData}>
+        {editId ? "Update" : "Add"}
+      </button>
+    </div>
+
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <span>{task.title}</span>
+
+          <div className="actions">
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+            <button onClick={() => editTask(task)}>Edit</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 }
 
 export default App;
